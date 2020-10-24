7767634f02e74327f3b70c0f90352e407cccf6ad
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-24 12:40:13 +0530

    Added:  all Functions  i.e. add, view,serch,delet.

--- src/com/org/AddressBook.java
+++ src/com/org/AddressBook.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-	
-	ArrayList persons;
 
-	public AddressBook()
-	{
+	ArrayList persons;
+	AddressBook ad;
+	UserInfo user;
+
+	public AddressBook() {
 		persons = new ArrayList();
 	}
 
-	public void addPerson() 
-	{	
+	public void addPerson() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter First Name: ");
 		String firstName = scan.nextLine();
@@ -28,50 +28,40 @@ public class AddressBook {
 		System.out.println("Enter zip code: ");
 		String zip = scan.nextLine();
 
-		UserInfo p = new UserInfo(firstName, lastName, address, phoneNumber , state, zip);
-		persons.add(p);
-		p.print();
+		user = new UserInfo(firstName, lastName, address, phoneNumber, state, zip);
+		persons.add(user);
 	}
-	
-	public void searchPerson(String n) 
-	{
-		for (int i = 0; i < persons.size(); i++) 
-		{
+
+	public void searchPerson(String n) {
+		for (int i = 0; i < persons.size(); i++) {
 			UserInfo p = (UserInfo) persons.get(i);
-			
-			if (n.equals(p.firstName)) 
-			{
-				p.print();
-			}
-			else
-			{
+
+			if (n.equals(p.getFirstName())) {
+				ad.view();// p.print();
+			} else {
 				System.out.println("Name Not Found !!!");
 			}
-		} 
-	} 
-		public void view()
-		{
-			for (int i = 0; i < persons.size(); i++)
-			{
-				UserInfo p = (UserInfo) persons.get(i);
-				p.print();
-			} 
-		} 
-		
-	 public void deletePerson (String n)
-	 {
-		 for (int i=0; i< persons.size(); i++) 
-		 {
-			 UserInfo p = (UserInfo)persons.get(i);
-			 if ( n.equals(p.firstName) ) 
-			 {
-				 persons.remove(i);
-				 System.out.println("User Removed Successfully");
-			 }
-			 else
-			{
+		}
+	}
+
+	public void view() {
+		System.out.println("FirstName    LastName     City     State   Contact      Zip");
+		for (int i = 0; i < persons.size(); i++) {
+
+			System.out.println(user.getFirstName() + " \t  " + user.getLastName() + " \t  " + user.getAddress()
+					+ " \t  " + user.getState() + " \t   " + user.getPhoneNumber() + " \t " + user.getZip());
+		}
+	}
+
+	public void deletePerson(String n) {
+		for (int i = 0; i < persons.size(); i++) {
+			UserInfo p = (UserInfo) persons.get(i);
+			if (n.equals(p.firstName)) {
+				persons.remove(i);
+				System.out.println("User Removed Successfully");
+			} else {
 				System.out.println("Name Not Found !!!");
 			}
-		 }
-	 }	 
+		}
+	}
 }
