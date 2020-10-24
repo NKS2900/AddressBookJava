2c5d1c772bf6723a4cb3161de4c95d7d695c47bb
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-24 12:42:04 +0530

    Added: Setter and Getter (POJO class)

--- src/com/org/UserInfo.java
+++ src/com/org/UserInfo.java
@@ -1,27 +1,68 @@
 package com.org;
 
-public class UserInfo 
-{
-
-	String firstName;
+public class UserInfo {
 	String lastName;
 	String address;
 	String phoneNumber;
 	String state;
 	String zip;
-	
-	UserInfo(String firstName , String lastName , String address , String phoneNumber , String state , String zip){
-		this.firstName=firstName;
+
+	UserInfo(String firstName, String lastName, String address, String phoneNumber, String state, String zip) {
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
 		this.state = state;
 		this.zip = zip;
-		
 	}
-	public void print() 
-	{
-		System.out.println(" ");
-		System.out.println("Firstname : " + firstName + "\nLastName : " + lastName + "\nAddress : " + address + "\nPhoneNumber : " + phoneNumber + "\nState : " + state + "\nZip Code : " + zip );
+
+	String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
 	}
 }
